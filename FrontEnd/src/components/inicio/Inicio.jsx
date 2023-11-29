d3098f45aef9b546e26c4902a7bf6585fec13d62
JairDel <delval.jair@gmail.com>
2023-11-28 19:59:07 -0600

    parte de abajo creada y modificacion de las imagenes

--- FrontEnd/src/components/inicio/Inicio.jsx
+++ FrontEnd/src/components/inicio/Inicio.jsx
@@ -8,18 +8,25 @@ const Inicio = () => {
             </div>
             <div className="parte_media">
                 <a href="skills.html" className="imagen_s">
-                    <img src="/ejemplo.jpg" alt=""/>
+                    <img src="/skills.jpg" alt=""/>
                     <span className="links">Skills</span>
                 </a>
                 <a href="weapons.html" className="imagen_s">
-                    <img src="/ejemplo.jpg" alt=""/>
+                    <img src="/armas.jpg" alt=""/>
                     <span className="links">Weapons</span>
                 </a>
                 <a href="loadout.html" className="imagen_s">
-                    <img src="/ejemplo.jpg" alt=""/>
+                    <img src="/loadout.jpg" alt=""/>
                     <span className="links">Loadout</span>
                 </a>
             </div>
+
+            <div className="parte_baja">
+                <a href="loadout.html" className="imagen_i">
+                        <img src="/records.jpg" alt=""/>
+                </a>
+            </div>
+
         </div>
     );
 }
