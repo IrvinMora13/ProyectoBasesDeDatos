c80d788d7b65175a3414e4078b7302c4ba08e8dd
Irvin Morales <a343423@gmail.com>
2023-11-29 13:34:18 -0600

    diseno tabla weapons

--- FrontEnd/src/components/weapons/Weapons.jsx
+++ FrontEnd/src/components/weapons/Weapons.jsx
@@ -3,6 +3,8 @@ import './weapons.css';
 
 const Weapons = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -10,8 +12,11 @@ const Weapons = () => {
         const response = await fetch('http://localhost:3001/api/data/Weapons');
         const result = await response.json();
         setData(result);
+        setLoading(false);
       } catch (error) {
         console.error('Error al obtener datos del servidor:', error);
+        setError('Error al obtener datos del servidor');
+        setLoading(false);
       }
     };
 
@@ -20,36 +25,41 @@ const Weapons = () => {
 
   return (
     <div>
-      <h1>Tabla de Datos</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Weapons ID</th>
-            <th>Weapons Name</th>
-            <th>Damage</th>
-            <th>Magazine</th>
-            <th>Recoil</th>
-            <th>Stability</th>
-            <th>Accuracy</th>
-            <th>Firing mode</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* Mapea los datos para crear filas en la tabla */}
-          {data.map((item) => (
-            <tr key={item[0]}>
-              <td>{item[0]}</td>
-              <td>{item[1]}</td>
-              <td>{item[2]}</td>
-              <td>{item[3]}</td>
-              <td>{item[4]}</td>
-              <td>{item[5]}</td>
-              <td>{item[6]}</td>
-              <td>{item[7]}</td>
+      <h1>Weapons Data Table</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Weapons ID</th>
+              <th>Weapons Name</th>
+              <th>Damage</th>
+              <th>Magazine</th>
+              <th>Recoil</th>
+              <th>Stability</th>
+              <th>Accuracy</th>
+              <th>Firing mode</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item[0]}>
+                <td>{item[0]}</td>
+                <td>{item[1]}</td>
+                <td>{item[2]}</td>
+                <td>{item[3]}</td>
+                <td>{item[4]}</td>
+                <td>{item[5]}</td>
+                <td>{item[6]}</td>
+                <td>{item[7]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
